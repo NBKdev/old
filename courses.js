96c07809478d28402a309d4c1fe0e4db407c6299
Your Name <you@example.com>
2022-11-17 20:30:57 +0000

    update

--- courses.js
+++ courses.js
@@ -2,7 +2,7 @@ let courses = [{
     id: 1001,
     title: "Japanese Lesson",
     description: "Japanese lessons tought by <em>professional</em> Japanese speakers!",
-    price: "£25",
+    price: "£20",
     loc: "Central London",
     spaces: 5,
     image: "assets/japanese.png"
