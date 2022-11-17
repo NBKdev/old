7c13aae18af094ec07e2e556f175042dcbe7c1fb
Your Name <you@example.com>
2022-11-17 20:25:18 +0000

    update dev

---
+++ courses.js
@@ -0,0 +1,91 @@
+let courses = [{
+    id: 1001,
+    title: "Japanese Lesson",
+    description: "Japanese lessons tought by <em>professional</em> Japanese speakers!",
+    price: "£20",
+    loc: "Central London",
+    spaces: 5,
+    image: "assets/japanese.png"
+},
+{
+    id: 1002,
+    title: "Maths",
+    description: "Maths Lesson by <em>professional</em> Learn how to math!",
+    price: "£25",
+    loc: "Central London",
+    spaces: 5,
+    image: "assets/math.png"
+},
+{
+    id: 1003,
+    title: "Geography Lesson",
+    description: "Geography Lessons by <em>professional</em> Know your maps!",
+    price: "£23.99",
+    loc: "Kent",
+    spaces: 5,
+    image: "assets/geo.png"
+},
+{
+    id: 1004,
+    title: "English Lesson",
+    description: "English lessons by <em>professional</em> English speakers!",
+    price: "£40",
+    loc: "Colchester",
+    spaces: 5,
+    image: "assets/english.png"
+},
+{
+    id: 1005,
+    title: "History Lesson",
+    description: "History lessons by <em>professional</em> learn about the victorians!",
+    price: "£39.99",
+    loc: "Norwich",
+    spaces: 5,
+    image: "assets/history.png"
+},
+{
+    id: 1006,
+    title: "Science Lesson",
+    description: "Science Lessons by <em>professional</em> Learn your physics!",
+    price: "£25",
+    loc: "Ipswich",
+    spaces: 5,
+    image: "assets/Science.png"
+},
+{
+    id: 1007,
+    title: "Art Lesson",
+    description: "Art Lessons by <em>professional</em> learn how to draw!",
+    price: "£20",
+    loc: "East London",
+    spaces: 5,
+    image: "assets/Art.png"
+},
+{
+    id: 1008,
+    title: "Phyiscal Education Lesson",
+    description: "PE Lessons by <em>professional</em> learn how to become healthy!",
+    price: "£25",
+    loc: "Central London",
+    spaces: 5,
+    image: "assets/PE.png"
+},
+{
+    id: 1009,
+    title: "Information Technology Lesson",
+    description: "IT Lessons by <em>professional</em> learn how to code!",
+    price: "£15",
+    loc: "Leeds",
+    spaces: 5,
+    image: "assets/IT.png"
+},
+{
+    id: 1010,
+    title: "Music Lesson",
+    description: "Music Lessons by <em>professional</em> learn how to sing!",
+    price: "£10",
+    loc: "West London",
+    spaces: 5,
+    image: "assets/Music.png"
+},
+];
